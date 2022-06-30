49402e8aa9eb4ad240eefa626470b8cdb8cebff0
sangluong2k2 <luongduysang2k2@gmail.com>
2022-06-30 12:39:29 +0700

    getstaticprops

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,11 +1,31 @@
+import { GetStaticProps } from 'next'
 import React from 'react'
 
-type Props = {}
+type ProductProps = {
+  products: any[];
+}
 
-const Products = (props: Props) => {
+const Products = ({products}: ProductProps) => {
   return (
-    <div>Products</div>
+    <div>
+      {products.map( item => (
+        <div key={item.id}> 
+          {item.name}
+        </div>
+      ))}
+    </div>
   )
+}
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
+  console.log('getStaticProps');
+  const responsive = await fetch('https://6110f09bc38a0900171f0ed0.mockapi.io/products');
+  const data = await responsive.json();
+  return {
+    props: {
+      products: data
+    }
+  }
 }
 
 export default Products
